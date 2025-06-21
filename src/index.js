235d862f915835b4d29e6a08c3552a5c37a05e3c
Simvol <144470909+S1mvolxD@users.noreply.github.com>
2025-06-21 11:32:33 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+const AioBase = require('./classes/AioBase');
+const AioBase = require('./classes/AioClient');
+const AioContext = require('./classes/AioContext');
+const AioParser = require('./classes/AioParser');
+const LoadCommands = require('./classes/LoadCommands');
+
 module.exports = {
-  AioBase: require('./classes/AioBase'),
-  AioClient: require('./classes/AioClient'),
-  AioContext: require('./classes/AioContext'),
-  AioParser: require('./classes/AioParser')
+  AioBase,
+  AioClient,
+  AioContext,
+  AioParser,
+  localStorage
 };
