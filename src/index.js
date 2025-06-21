244252b64b69d95efb638ecc12ca60d5ae253019
Simvol <144470909+S1mvolxD@users.noreply.github.com>
2025-06-21 12:46:33 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const AioBase = require('./classes/AioBase');
-const AioClient = require('./classes/AioClient');
+const AioBase = require('./classes/AioClient');
 const AioContext = require('./classes/AioContext');
 const AioParser = require('./classes/AioParser');
 const LoadCommands = require('./classes/LoadCommands');
+const AioError = require('./classes/AioError');
 
 module.exports = {
   AioBase,
   AioClient,
   AioContext,
   AioParser,
+  AioError,
   LoadCommands
 };
