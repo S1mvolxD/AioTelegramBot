f38e85f40759fc6673125077a2a91a0a85a87b4f
Simvol <144470909+S1mvolxD@users.noreply.github.com>
2025-06-21 10:13:15 +0700

    Update AioParser.js

--- src/classes/AioParser.js
+++ src/classes/AioParser.js
@@ -1,15 +1,21 @@
 class AioParser {
-  static parseBlocks(code, context, actions) {
+  static async parseBlocks(code, context, actions) {
     const blockRegex = /\$(\w+)\[([^\]]+)\]/g;
-    let match;
     const promises = [];
+    let match;
 
     while ((match = blockRegex.exec(code)) !== null) {
       const [fullMatch, actionName, arg] = match;
       const action = actions[actionName];
       
       if (action) {
-        promises.push(action(context, arg));
+        try {
+          promises.push(action(context, arg));
+        } catch (e) {
+          console.error(`Error executing ${actionName}:`, e);
+        }
+      } else {
+        console.warn(`Unknown action: ${actionName}`);
       }
     }
 
